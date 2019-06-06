56eabe767778e28a622e1408d0f9cae0804c1943
Mattias Walla <mattias.walla@gmail.com>
2019-06-07 01:22:40 +0200

    alu_weights

--- extract_data.py
+++ extract_data.py
@@ -8,6 +8,14 @@ NEW_SIZE = SIZE - 882
 CORES = 15
 N_KERNELS = 14
 
+
+def increment_or_initiate(d, key, value):
+    if key in d:
+        d[key] += value
+    else:
+        d[key] = value
+
+
 def read_speedup():
 
     times = np.zeros((SIZE,CORES), dtype=float)
@@ -95,36 +103,136 @@ def remove_mm_kernel(speedups, times, threads, blocks, run_order):
     return new_speedups, new_times, new_threads, new_blocks, new_run_order
 
 
+def draw_histogram(speedups, times, size ):
+    ###### Histogram #####
+    max_speedup = np.ones((size))
+    for i in range(size):
+        for j in range(1, CORES):
+            if speedups[i, j] > max_speedup[i]:
+                max_speedup[i] = speedups[i, j]
+
+    largest_speedup = math.ceil(max(max_speedup))
+
+    '''
+    accumulated_time= np.zeros(largest_speedup)
+    for i in range(SIZE):
+        index = math.ceil(max_speedup[i])-1
+        accumulated_time[index] += data[i,0]/1000000
+    print (accumulated_time)
+
+    '''
+
+    bins = [x for x in range(largest_speedup + 1)]
+    print(bins)
+
+    _ = plt.hist(max_speedup, bins=bins, weights=[times[i, 0] / 1000000 for i in range(size)])
+    _ = plt.xlabel('Maximal speedup')
+    _ = plt.ylabel('Kernel time [s]')
+    plt.show()
+
+
+
 count, names = read_ptx()
 speedups, times, run_order = read_speedup()
 threads, blocks = read_tb()
 
 speedups, times, threads, blocks, run_order = remove_mm_kernel(speedups, times, threads, blocks, run_order)
 
-###### Histogram #####
+#draw_histogram(speedups,times, NEW_SIZE)
 
-max_speedup= np.ones((NEW_SIZE))
-for i in range(NEW_SIZE):
-    for j in range (1, CORES):
-        if speedups[i,j] > max_speedup[i]:
-            max_speedup[i] = speedups[i,j]
 
-largest_speedup = math.ceil(max(max_speedup))
 
-'''
-accumulated_time= np.zeros(largest_speedup)
-for i in range(SIZE):
-    index = math.ceil(max_speedup[i])-1
-    accumulated_time[index] += data[i,0]/1000000
-print (accumulated_time)
+sum_count = [dict() for _ in range(14)]
+remaining = [dict() for _ in range(14)]
+for i in range(14):
+    for key, value in count[i].items():
+        if 'ld.param' in key and '32' in key:
+            increment_or_initiate(sum_count[i], 'ld.param', value * 32)
+        elif 'ld.param' in key and '64' in key:
+            increment_or_initiate(sum_count[i], 'ld.param', value * 64)
+        elif 'st.param' in key and '32' in key:
+            increment_or_initiate(sum_count[i], 'st.param', value * 32)
+        elif 'st.param' in key and '64' in key:
+            increment_or_initiate(sum_count[i], 'st.param', value * 64)
+        elif 'or.' in key or 'and.' in key or 'not' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 9)
+        elif 'setp.' in key and '32' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 9)
+        elif 'cvta.' in key or 'mov.' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 9)
+        elif 'cvt.' in key or 'mov.' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 9)
+        elif ('mul.' in key or 'add.' in key or 'sub.' in key or 'fma.' in key or 'min.' in key or 'max.' in key or 'neg.' in key or 'selp.' in key or 'shl.' in key or 'abs.' in key) and '32' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 9)
+        elif ('mul.' in key or 'add.' in key or 'sub.' in key or 'fma.' in key or 'min.' in key or 'max.' in key or 'neg.' in key or 'selp.' in key or 'shl.' in key or 'abs.' in key) and '64' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 10)
+        elif 'st.shared' in key and '32' in key:
+            increment_or_initiate(sum_count[i], 'st.shared', value * 32)
+        elif 'st.shared' in key and '64' in key:
+            increment_or_initiate(sum_count[i], 'st.shared', value * 64)
+        elif 'ld.shared' in key and '32' in key:
+            increment_or_initiate(sum_count[i], 'ld.shared', value * 32)
+        elif 'ld.shared' in key and '64' in key:
+            increment_or_initiate(sum_count[i], 'ld.shared', value * 64)
+        elif 'st.global' in key and '8' in key:
+            increment_or_initiate(sum_count[i], 'st.global', value * 8)
+        elif 'st.global' in key and '16' in key:
+            increment_or_initiate(sum_count[i], 'st.global', value * 16)
+        elif 'st.global' in key and '32' in key:
+            increment_or_initiate(sum_count[i], 'st.global', value * 32)
+        elif 'st.global' in key and '64' in key:
+            increment_or_initiate(sum_count[i], 'st.global', value * 64)
+        elif 'ld.global' in key and '16' in key:
+            increment_or_initiate(sum_count[i], 'ld.global', value * 16)
+        elif 'ld.global' in key and '32' in key:
+            increment_or_initiate(sum_count[i], 'ld.global', value * 32)
+        elif 'ld.global' in key and '64' in key:
+            increment_or_initiate(sum_count[i], 'ld.global', value * 64)
+        elif 'bar.' in key:
+            increment_or_initiate(sum_count[i], 'CTA sync', value)
+        elif 'div.s' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 149)
+        elif 'div.u' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 131)
+        elif 'div.' in key and 'f32' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 418)
+        elif 'div.' in key and 'f64' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 445)
+        elif 'mad.' in key and '32' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 9)
+        elif 'mad.' in key and '64' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 10)
+        elif 'rem.u32' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 116)
+        elif 'mul24.' in key or 'mad24.' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 22)
+        elif 'rsqrt.approx' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 40)
+        elif 'sqrt.approx' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 49)
+        elif 'sqrt.' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 432)
+        elif 'ex2.approx' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 49)
+        elif 'rcp.' in key:
+            increment_or_initiate(sum_count[i], 'arith', value * 377)
+        elif 'bra.' in key:
+            increment_or_initiate(sum_count[i], 'branch', value)
+        elif 'ld.' in key and '32' in key:
+            increment_or_initiate(sum_count[i], 'ld.param', value * 32)
+        elif 'ld.' in key and '64' in key:
+            increment_or_initiate(sum_count[i], 'ld.param', value * 64)
+        elif 'st.' in key and '32' in key:
+            increment_or_initiate(sum_count[i], 'st.param', value * 32)
+        elif 'st.' in key and '64' in key:
+            increment_or_initiate(sum_count[i], 'st.param', value * 64)
+        else:
+            (remaining[i])[key] = value
 
-'''
+print(remaining)
+for i in range(12):
+    print(sum_count[i])
 
-bins = [x for x in range(largest_speedup+1)]
-print (bins)
 
-_ = plt.hist(max_speedup, bins=bins, weights=[times[i,0]/1000000 for i in range(NEW_SIZE)])
-_ = plt.xlabel('Maximal speedup')
-_ = plt.ylabel('Kernel time [s]')
-plt.show()
+
 
